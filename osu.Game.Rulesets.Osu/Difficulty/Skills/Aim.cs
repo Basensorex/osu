a406337859eba27c7a06aaf787a799bc4925e01a
basen <basensorex@gmail.com>
2026-03-27 19:02:51 +0000

    welcome back d/t^3

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -35,7 +35,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 
         private double skillMultiplierSnap => 71.0;
         private double skillMultiplierAgility => 2.35;
-        private double skillMultiplierFlow => 253.0;
+        private double skillMultiplierFlow => 255.0;
         private double skillMultiplierTotal => 1.11;
         private double meanExponent => 1.2;
 
